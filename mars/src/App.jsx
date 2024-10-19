c8b2cc5c5dcc36608104bf6ecdc56dd1cf0bc3bd
mmfx001 <dilbekshermatov@gmail.com>
2024-10-19 18:18:14 +0500

    done

--- mars/src/App.jsx
+++ mars/src/App.jsx
@@ -2,16 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Admin from './admin';
+import AdminPanel from './page/shopedith';
 import AdminForcheck from './page/AdminForcheck'
+import UsersEdit from './page/usersedith';
 import TeachersEdit from './page/teacher';
-
+import Adminn from './page/AdminRaiting';
+import UserDetail from './page/userDetail';
 
 function App() {
     return (
         <div>
             {/* Routes Setup */}<Admin/>
             <Routes>
-          
+                <Route path='/' element={<UsersEdit />} />
+                <Route path='/shop' element={<AdminPanel />} />
+                <Route path='/adminr' element={<Adminn />} />
+                <Route path='/detail' element={<UserDetail />} />
                 <Route path='/teacher' element={<TeachersEdit />} />
                 <Route path='/admin' element={<AdminForcheck />} />
                 <Route path="*" element={"note data"} /> {/* 404 sahifasi */}
