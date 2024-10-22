0bfb90102b5f869f8b3a719de5bad9d18aa5e883
mmfx001 <dilbekshermatov@gmail.com>
2024-10-22 14:35:52 +0500

    done

--- mars/src/App.jsx
+++ mars/src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React  from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Admin from './admin';
+import AdminPanel from './page/shopedith';
+import AdminForcheck from './page/AdminForcheck'
+import UsersEdit from './page/usersedith';
+import TeachersEdit from './page/teacher';
+import Adminn from './page/AdminRaiting';
+import UserDetail from './page/userDetail';
 
 function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <div>
+            {/* Routes Setup */}<Admin/>
+    <div className='mt-24'>
+    <Routes>
+                <Route path='/' element={<UsersEdit />} />
+                <Route path='/shop' element={<AdminPanel />} />
+                <Route path='/adminr' element={<Adminn />} />
+                <Route path='/detail' element={<UserDetail />} />
+                <Route path='/teacher' element={<TeachersEdit />} />
+                <Route path='/admin' element={<AdminForcheck />} />
+                <Route path="*" element={"note data"} /> {/* 404 sahifasi */}
+            </Routes>
+    </div>
+        </div>
+    );
 }
 
-export default App
+
+
+export default App;
--- mars/src/main.jsx
+++ mars/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 import store from './store'; // Ensure your store is correctly configured in './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <Router>
+    <BrowserRouter> {/* Bu yerda BrowserRouter dan foydalaning */}
       <App />
-    </Router>
+    </BrowserRouter>
   </Provider>
 );
