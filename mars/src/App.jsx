c1a91b7cfbf3474d09c92dc64abd70f9d57dda49
mmfx001 <dilbekshermatov@gmail.com>
2024-10-22 14:28:14 +0500

    done

--- mars/src/App.jsx
+++ mars/src/App.jsx
@@ -1,31 +1,9 @@
-import React  from 'react';
-import { Route, Routes } from 'react-router-dom';
-
-import Admin from './admin';
-import AdminPanel from './page/shopedith';
-import AdminForcheck from './page/AdminForcheck'
-import UsersEdit from './page/usersedith';
-import TeachersEdit from './page/teacher';
-import Adminn from './page/AdminRaiting';
-import UserDetail from './page/userDetail';
+import React from 'react'
 
 function App() {
-    return (
-        <div>
-            {/* Routes Setup */}<Admin/>
-            <Routes>
-                <Route path='/' element={<UsersEdit />} />
-                <Route path='/shop' element={<AdminPanel />} />
-                <Route path='/adminr' element={<Adminn />} />
-                <Route path='/detail' element={<UserDetail />} />
-                <Route path='/teacher' element={<TeachersEdit />} />
-                <Route path='/admin' element={<AdminForcheck />} />
-                <Route path="*" element={"note data"} /> {/* 404 sahifasi */}
-            </Routes>
-        </div>
-    );
+  return (
+    <div>App</div>
+  )
 }
 
-
-
-export default App;
+export default App
